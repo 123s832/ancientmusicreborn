0d6db9692933f3febd8d0feba001e7e2f603f3e0
123s832 <2216131752@qq.com>
2026-04-19 17:14:16 +0800

    Add files via upload

---
+++ .github/workflows/vite.config.ts
@@ -0,0 +1,41 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+// https://vite.dev/config/
+export default defineConfig({
+  appType: 'spa',
+  plugins: [
+    {
+      name: 'serve-index-html-at-root',
+      configureServer(server) {
+        server.middlewares.use((req, _res, next) => {
+          if (req.url === '/' || req.url === '') req.url = '/index.html'
+          next()
+        })
+      },
+    },
+    react({
+      babel: {
+        plugins: [
+          'react-dev-locator',
+        ],
+      },
+    }),
+    tsconfigPaths(),
+  ],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3005',
+        changeOrigin: true,
+        secure: false,
+        configure: (proxy) => {
+          proxy.on('error', (err) => {
+            console.log('proxy error', err)
+          })
+        },
+      }
+    },
+  },
+})
